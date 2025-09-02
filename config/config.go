77ef75046bc1c37327747ba764035ccaa875aa55
KhumodiatlaM <khumodiatlaM@tfg.co.za>
2025-09-02 18:44:53 +0200

    Add GetDBConnectionString method

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"github.com/spf13/viper"
 )
 
@@ -29,4 +30,10 @@ func LoadConfig() (Config, error) {
 
 	err = viper.Unmarshal(&config)
 	return config, err
+}
+
+// GetDBConnectionString constructs the database connection string
+func (cfg *Config) GetDBConnectionString() string {
+	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName)
 }
