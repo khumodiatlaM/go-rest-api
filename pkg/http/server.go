72a885354cf7e64dae135fe1a69c5f4b4d58413d
KhumodiatlaM <khumodiatlaM@tfg.co.za>
2025-09-05 18:00:18 +0200

    add start server function

---
+++ pkg/http/server.go
@@ -0,0 +1,32 @@
+package http
+
+import (
+	"fmt"
+	"go-rest-api/pkg/logger"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func StartServer(port string, router *httprouter.Router, logger logger.Logger) {
+	// validate port
+	if port == "" {
+		port = "8080" // default port
+	}
+	// validate port is an number
+	_, err := strconv.Atoi(port)
+	if err != nil {
+		logger.Fatal("Port must be a number.")
+	}
+	logger.Info("Starting http server on port " + port)
+
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: router,
+	}
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatal("Failed to start http server", "error", err)
+	}
+}
