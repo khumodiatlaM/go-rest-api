8a158adfe2b076cf111c17884b07fb6bab3ae04e
KhumodiatlaM <khumodiatlaM@tfg.co.za>
2025-09-05 20:30:31 +0200

    add MockUserService

--- internal/core/mocks.go
+++ internal/core/mocks.go
@@ -6,6 +6,10 @@ import (
 	"github.com/stretchr/testify/mock"
 )
 
+// ---------------------------------
+// MockUserRepository
+// ---------------------------------
+
 type MockUserRepository struct {
 	mock.Mock
 }
@@ -17,6 +21,27 @@ func (r *MockUserRepository) CreateUser(ctx context.Context, user *User) error {
 
 func (r *MockUserRepository) GetUserByID(ctx context.Context, id string) (*User, error) {
 	args := r.Called(ctx, id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*User), args.Error(1)
+}
+
+// ---------------------------------
+// MockUserService
+// ---------------------------------
+
+type MockUserService struct {
+	mock.Mock
+}
+
+func (s *MockUserService) CreateUser(ctx context.Context, user *User) error {
+	args := s.Called(ctx, user)
+	return args.Error(0)
+}
+
+func (s *MockUserService) GetUserByID(ctx context.Context, id string) (*User, error) {
+	args := s.Called(ctx, id)
 	if args.Get(0) == nil {
 		return nil, args.Error(1)
 	}
