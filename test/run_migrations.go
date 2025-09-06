9284147712a6f1f56574378b5ca3a3badb1f3a61
KhumodiatlaM <khumodiatlaM@tfg.co.za>
2025-09-06 12:55:03 +0200

    remove a log

--- test/run_migrations.go
+++ test/run_migrations.go
@@ -13,5 +13,4 @@ func RunMigrations(t *testing.T, dsn string) {
 	if err != nil {
 		t.Fatalf("Failed to run migrations: %v, output: %s", err, string(output))
 	}
-	t.Logf("Successfully ran migrations: %s", string(output))
 }
